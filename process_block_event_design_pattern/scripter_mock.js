62b4eefa4a970df398807eb0a04f1844fc03348b
Pilcrow Records <99051913+pilcrowrecords@users.noreply.github.com>
2022-06-27 09:55:27 -0400

    Added initial framework
    Works versus leftCycleBeat, but rightCycleBeat doesn't work in 2 cases: when the note length >= the cycle length; some unknown error when calculating beats. Also includes a line which aligns the TRIGGER value to musically useful increments.

--- process_block_event_design_pattern/scripter_mock.js
+++ process_block_event_design_pattern/scripter_mock.js
@@ -29,8 +29,29 @@ THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR I
 	INIT MOCK
 */
 
-const PROCESS_BLOCKS = [];
+var src_pb = require('./process_blocks_1_5_cycling.js');
+var note_lib = require('./note_lengths.js');
+
+const PROCESS_BLOCKS = src_pb.pb_1_5_cy;
 var CURRENT_BLOCK = {};
+
+var NOTE_VALUE = 0;
+const NOTE_LENGTH_KEYS = Object.keys(note_lib.NOTE_LENGTHS_LIB);
+//const KEYS_LENGTH = NOTE_LENGTH_KEYS.length;
+
+const RESET_VALUE = -1;
+var CURSOR = RESET_VALUE;
+var CURSOR_STEP = 0.01; // smallest note length = 0.25
+// applied when the track stops playing
+var TRIGGER = RESET_VALUE;
+
+NOTE_LENGTH_KEYS.forEach( function( key ) {
+	NOTE_VALUE = note_lib.NOTE_LENGTHS_LIB[key];
+	Trace("\n**** " + key + " ****\n");
+	play();
+	CURSOR = RESET_VALUE;
+	TRIGGER = RESET_VALUE
+});
 
 /*
 	SCRIPTER MOCK ENVIRONMENT
@@ -42,8 +63,75 @@ function HandleMIDI( event ) {
 }
 
 function ProcessMIDI() {
-	var timingInfo = GetTimingInfo();
-	Trace( "timingInfo" );
+	var timing_info = GetTimingInfo();
+	if ( timing_info.playing ) {
+		// get the cursor to within the bounds of the track or the cycle
+		if ( timing_info.cycling ) {
+			CURSOR = ( timing_info.blockStartBeat < timing_info.leftCycleBeat ? timing_info.leftCycleBeat : timing_info.blockStartBeat );
+		} else {
+			CURSOR = timing_info.blockStartBeat;
+		}
+
+		CURSOR = Math.ceil( CURSOR * note_lib.NOTE_LENGTH_MAX ) / note_lib.NOTE_LENGTH_MAX;
+
+		// lay down the trigger on the cursor if the track is starting up again
+		if ( TRIGGER == RESET_VALUE ) {
+			TRIGGER = CURSOR;
+		}
+		// move the cursor along the process block while keeping within the bounds of the cycle
+		var END_BEAT = ( timing_info.blockEndBeat > timing_info.rightCycleBeat ? timing_info.rightCycleBeat : timing_info.blockEndBeat );
+
+		while ( CURSOR <= END_BEAT ) {
+			// Trace( CURSOR + "\t" + TRIGGER );
+			if ( TRIGGER < timing_info.leftCycleBeat ) {
+				Trace( "lcb:" + timing_info.leftCycleBeat + "\t" + "t:" + TRIGGER );
+			} else if ( TRIGGER > timing_info.rightCycleBeat ) {
+				Trace( "t:" + TRIGGER + "\trcb:" + timing_info.leftCycleBeat );
+			} else {
+				// do nothing
+			}
+
+			if ( CURSOR == TRIGGER ) {
+				// create and send the NoteOn event on the cursor beat
+				
+				var note_off_beat = CURSOR + NOTE_VALUE;
+				// if the note off lands outside the bounds of the cycle, then shift the beat to relative to the beginning of the cycle
+				/* 
+				TODO: The trigger can still land outside the bounds of the cycle if
+					* the note value is the greater than or equal to the cycle length
+					* the math is not double checked for certain note values
+				*/
+				if ( timing_info.cycling && note_off_beat > timing_info.rightCycleBeat ) {
+					/* FIRST ATTEMPT; works in most cases
+					// get the difference between the right cycle beat and the note off beat
+					var overage = note_off_beat - timing_info.rightCycleBeat;
+					// add the difference to the left cycle beat
+					var offset = overage + timing_info.leftCycleBeat;
+					// drop the note off event at that shifted beat
+					note_off_beat = offset;
+					*/
+
+					/* SECOND ATTEMPT
+						Would modulus work here?
+					*/
+				}
+				// set the trigger on the NoteOff beat
+				TRIGGER = note_off_beat;
+			}
+			CURSOR += CURSOR_STEP;
+		}
+	} else {
+		// the track stopped playing.
+		// reset the trigger so we don't get any events in unwanted beats
+		TRIGGER = RESET_VALUE;
+		CURSOR = RESET_VALUE;
+		// TODO: turn off any notes which may be playing
+	}
+}
+
+function rInt( min, max ) {
+	if (min == max ) {return min;}
+    return Math.floor(min + Math.random()*(max + 1 - min));
 }
 
 /*
@@ -53,7 +141,6 @@ function ProcessMIDI() {
 
 // runs through a typical sequence of events
 // callback functions are called by type
-play();
 function play() {
 
 	PROCESS_BLOCKS.forEach( function ( process_block ) {
