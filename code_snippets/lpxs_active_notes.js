072f3678780bb4389c52577ea57834e5a55b4603
Pilcrow Records <99051913+pilcrowrecords@users.noreply.github.com>
2022-08-06 21:12:09 -0400

    Update lpxs_active_notes.js

--- code_snippets/lpxs_active_notes.js
+++ code_snippets/lpxs_active_notes.js
@@ -14,3 +14,23 @@ function HandleMIDI( event ) {
 	}
 	event.send();
 }
+
+var ACTIVE_NOTES = {};
+
+function HandleMIDI( event ) {
+const pitch = event.pitch;
+	if ( event instanceof NoteOn ) {
+		var notes = ACTIVE_NOTES[ pitch ];
+		if ( !notes ) {
+			notes = [];
+		}
+		notes.push( event );
+		ACTIVE_NOTES[ pitch ] = notes;
+	} else if ( event instanceof NoteOff ) {
+		var notes = ACTIVE_NOTES[ pitch ];
+		if ( notes ) {
+			var note = notes.pop();
+			ACTIVE_NOTES[ pitch ] = notes;
+		}
+	}
+}
