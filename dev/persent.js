1685e01fce2b989984092bdb30d0f2dde4342ce2
Pilcrow Records <99051913+pilcrowrecords@users.noreply.github.com>
2023-03-18 17:28:35 -0400

    Update persent.js

--- dev/persent.js
+++ dev/persent.js
@@ -21,7 +21,10 @@ var USAGE_COUNTS = {
    "leaps" : 0 // max 1
 };
 const MAX_PITCH = 8;
-const MIN_PITCH = -7;
+const MIN_PITCH = 1;
+const STEP = 1;
+const SKIP = 2;
+const LEAP = 3;
 
 var sequence = [];
 
@@ -29,12 +32,19 @@ var sequence = [];
 // determine highest and lowest notes
 
 var max = rInt(4, 8);
-var min = rInt(4, 7);
-min = 0 - min;
+var min = 0;
+if ( rInt( 0, 1 ) ) {
+    min = rInt(0, 7);
+    min = 0 - min;
+} else {
+    min = rInt(1, 4);
+}
 
 // select root note
 var start_note = START_OPTIONS[rInt(0, 2)];
 sequence.push(start_note);
+
+sequence.push(KEYSTONES.unique);
 
 // determine locations of highest and lowest notes
 
@@ -53,20 +63,52 @@ cadence.forEach( function ( pitch ) {
    sequence.push( pitch ); 
 });
 
+// place the unique pitch somewhere
+
 console.log(sequence);
 
 // between each post, determine the best approach
 
-var cursor = 0;
 var connectors = [];
 var connector = [];
 
 for (let index = 1; index < sequence.length; index++) {
-    connector = 0;
+    connector = [];
     let post_start = sequence[ index - 1 ];
     let post_end = sequence[ index ];
-    
+    let cache = post_start;
+    let distance = Math.abs ( post_start - post_end );
+    if ( distance == 0 ) {
+        // the notes are the same, so let's create some motion
+        // up or down
+        // step, skip, or leap
+        let up = rInt(0,1);
+        let motion = rInt(STEP,LEAP);
+        if ( motion == LEAP ) {
+            USAGE_COUNTS.leaps = 1;
+        }
+        if ( !up ) {
+            motion = 0 - motion;
+        }
+        connector.push(post_start += motion )
+    } else {
+        if ( distance >= leap && USAGE_COUNTS.leaps == 0 ) {
+            if ( distance > leap ) {
+                cache += 2;
+                connector.push(cache);
+            }
+            USAGE_COUNTS.leaps = 1;
+        }
+        while ( cache != post_end ) {
+            // fill the gap with skips and leaps
+            // check to see if a skip or step will land before post_end
+            let cache_post_distance = Math.abs ( post_end - cache ); 
+        }
+    }
+    connectors.push( connector );
 }
+
+// splice in the connectors
 
 function rInt( min, max ) {
 	if (min == max ) {return min;}
