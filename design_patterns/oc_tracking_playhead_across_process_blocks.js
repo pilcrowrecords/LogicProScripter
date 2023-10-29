da4d3c67b44859d93c0c924b9079d86361bb8228
Pilcrow Records <99051913+pilcrowrecords@users.noreply.github.com>
2023-10-29 16:36:13 -0400

    significant update to tracking which is simpler and less buggy
    Brings the code more in line with the source code from `Drum Probability Sequencer' while simplifying to the core calculations and moving the calculations out of the ProcessMIDI() function so there are fewer distractions to unique code.

--- design_patterns/oc_tracking_playhead_across_process_blocks.js
+++ design_patterns/oc_tracking_playhead_across_process_blocks.js
@@ -1,50 +1,106 @@
+/*
+This design pattern is based on the pattern found in the demo 
+Drum Probability Sequencer script. This accomplishes two things:
+* accurately tracks the next beat to be scheduled based on the current
+process block
+* Manages a trigger value so that events can be created as needed
+*/
+
+//needed to call GetTimingInfo()
 var NeedsTimingInfo = true;
+ResetParameterDefaults = true;
+
+// Used by beatToSchedule and TRIGGER to align musically
+// determines how many notes are in the time siqnature denominator
+// 0.25 = 1/1 note, 1 = 1/4 note, 4 = 1/16, 8 = 1/32
+const TIME_SIG_DENOM_DIVISION = 16; // beatToSchedule results in 1/64 notes
+const CURSOR_INCREMENT = 0.0001;
+
+var NOTE_LENGTHS = {
+	"1/64" : 0.0625,
+    "1/32" : 0.125,
+    "1/16" : 0.250,
+    "1/8"  : 0.500,
+    "1/4"  : 1.000, // in beatToSchedule
+    "1/2"  : 2.000,
+    "1/1"  : 4.000
+};
+var NOTE_LENGTH = NOTE_LENGTHS["1/4"];
 
 const RESET_VALUE = -1.0;
 var TRIGGER = RESET_VALUE;
-const CURSOR_INCREMENT = 0.001;
 
 function ProcessMIDI() {
-	var timing_info = GetTimingInfo();
 
-	if ( !timing_info.playing ){
-		cursor = timing_info.blockStartBeat;
-		TRIGGER = RESET_VALUE;
-		return;
-	}
-	
-	var lookAheadEnd = timing_info.blockEndBeat;
-	var cursor = timing_info.blockStartBeat;
-	if ( TRIGGER == RESET_VALUE ) {
-		TRIGGER = cursor;
-	}
-	
-	if ( timing_info.cycling && ( !TRIGGER || TRIGGER > timing_info.rightCycleBeat ) ) {
-		TRIGGER = ( timing_info.rightCycleBeat > timing_info.blockEndBeat ? timing_info.rightCycleBeat : timing_info.blockEndBeat ); 
-		if ( TRIGGER == timing_info.rightCycleBeat && Math.trunc(cursor) == timing_info.leftCycleBeat ) {
-			TRIGGER = timing_info.blockStartBeat;
-		}
-	}
-
-	if ( timing_info.cycling && lookAheadEnd >= timing_info.rightCycleBeat ) {
-			var cycleBeats = timing_info.rightCycleBeat - timing_info.leftCycleBeat;
-			var cycleEnd = lookAheadEnd - cycleBeats;
-	}
-
-	while ((cursor >= timing_info.blockStartBeat && cursor < lookAheadEnd)
-	|| (timing_info.cycling && cursor < cycleEnd)) {
-		if (timing_info.cycling && cursor >= timing_info.rightCycleBeat) {
-			cursor -= (timing_info.rightCycleBeat - timing_info.leftCycleBeat);
-			TRIGGER = cursor;
-		}
-
-		if ( cursor == TRIGGER ) {
-			// do something; create and manage events here
-		}
+    var timing_info = GetTimingInfo();
+    
+	if ( timing_info.playing ) {
 		
-		cursor += CURSOR_INCREMENT;
-		if ( TRIGGER < cursor ) {
-			TRIGGER = cursor;
-		}	
-	}
+        // init the values to calculate beats
+        var beatToSchedule = align_beat_to_bar_division( timing_info.blockStartBeat, TIME_SIG_DENOM_DIVISION );
+
+        if ( TRIGGER == RESET_VALUE ) {
+            TRIGGER = beatToSchedule;
+        }
+				
+        // loop through the beats that fall within this buffer
+        while ( beats_fall_within_buffer( beatToSchedule, timing_info ) ) {
+            // adjust for cycle
+            beatToSchedule = handle_beat_wraparound( beatToSchedule, timing_info );
+            TRIGGER = handle_beat_wraparound( TRIGGER, timing_info );
+
+            if ( beatToSchedule == TRIGGER ) {
+
+                // DO SOMETHING
+                var on = new NoteOn;
+                on.pitch = 60 + beatToSchedule;
+                on.velocity = 100;
+                on.sendAtBeat(beatToSchedule);
+                var off = new NoteOff(on);
+                off.sendAtBeat(beatToSchedule + NOTE_LENGTH);
+
+                // advance the trigger
+                TRIGGER += NOTE_LENGTH;
+            }
+
+
+            // advance to next beat
+            beatToSchedule += CURSOR_INCREMENT;
+            beatToSchedule = align_beat_to_bar_division( beatToSchedule, TIME_SIG_DENOM_DIVISION );
+		}
+	} else {
+        // .playing == false; continuous loop with no way to stop
+
+        // ensure the trigger aligns with the playhead on the next play
+        TRIGGER = RESET_VALUE;
+    }
+}
+
+
+// aligns any float value to the beats
+// ceiling used because all recordable beats are >= 1.000
+function align_beat_to_bar_division( value, division ) {
+    return Math.ceil( value * division ) / division;
+}
+
+// when the intended beat falls outside the cycle, wrap it proportionally 
+// from the cycle start
+function handle_beat_wraparound( value, timing_info ) {
+    if ( timing_info.cycling && value >= timing_info.rightCycleBeat ) {
+        value -= ( timing_info.rightCycleBeat - timing_info.leftCycleBeat );
+    }
+    return value;
+}
+
+// loop through the beats that fall within this buffer
+// including beats that wrap around the cycle point
+// return false by default
+function beats_fall_within_buffer ( beatToSchedule, timing_info ) {
+    let lookAheadEnd = timing_info.blockEndBeat;
+    let cycleBeats = timing_info.rightCycleBeat - timing_info.leftCycleBeat;
+    let cycleEnd = lookAheadEnd - cycleBeats;
+    if ( beatToSchedule >= timing_info.blockStartBeat && beatToSchedule < lookAheadEnd || (timing_info.cycling && beatToSchedule < cycleEnd)) {
+        return true;
+    }
+    return false;
 }
