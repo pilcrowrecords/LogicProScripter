f2bfeb5ffd1f2dd3f6b816df17e224e41f13122b
Pilcrow Records <99051913+pilcrowrecords@users.noreply.github.com>
2024-04-16 18:20:18 -0400

    added FOSS license

--- example_scripts/random_devices.js
+++ example_scripts/random_devices.js
@@ -6,6 +6,43 @@ with a transposition plug-in
 
 Roadmap:
 * Add default chord selections
+
+This script is released under the MIT License.
+
+    Permissions
+    * Commercial use
+    * Modification
+    * Distribution
+    * Private use
+
+    Limitations
+    x Liability
+    x Warranty
+
+    Conditions
+    ! License and copyright notice
+
+    Copyright Philip Regan and Pilcrow Records
+
+    Permission is hereby granted, free of charge, to any person obtaining
+    a copy of this software and associated documentation files (the
+    "Software"), to deal in the Software without restriction, including
+    without limitation the rights to use, copy, modify, merge, publish,
+    distribute, sublicense, and/or sell copies of the Software, and to
+    permit persons to whom the Software is furnished to do so, subject to
+    the following conditions:
+
+    The above copyright notice and this permission notice shall be
+    included in all copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+    EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+    MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+    NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+    LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+    OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+    WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+    
 ******************************************************************************/
 
 /*
